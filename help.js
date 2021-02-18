3228fe8e2be97836fd7735e0d93e0d1d549ab086
legend-js <75608223+legend-js-dev@users.noreply.github.com>
2021-02-18 19:43:58 +0545

    Add files via upload

---
+++ help.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js")
+module.exports = {
+  name: "help",
+  run: async (client, message, args) => {
+    const embed = new Discord.MessageEmbed()
+    embed.setTitle("Help | " + client.user.username)
+    embed.setThumbnail(client.user.displayAvatarURL())
+    embed.setFooter(client.user.tag + " | made by LΣGΣПD#0001", client.user.displayAvatarURL())
+    embed.setTimestamp()
+    embed.setDescription(`The commands are listed below:`)
+    embed.addField("anti-swear", '`addword` | `delword` | `set-warn-msg` | `words`')
+    embed.addField("info", '`help` | `ping`')
+    embed.setColor("GREEN")
+    message.channel.send({ embed: embed })
+  }
+}
