d10cf9a107f6969ba83407245e7e404884711a05
legend-js <75608223+legend-js-dev@users.noreply.github.com>
2021-06-21 19:33:52 +0545

    xd

--- commands/info/help.js
+++ commands/info/help.js
@@ -5,7 +5,7 @@ module.exports = {
     const embed = new Discord.MessageEmbed()
     embed.setTitle("Help | " + client.user.username)
     embed.setThumbnail(client.user.displayAvatarURL())
-    embed.setFooter(client.user.tag + " | made by LΣGΣПD#0001", client.user.displayAvatarURL())
+    embed.setFooter(client.user.tag + " | made by legendjs#0001", client.user.displayAvatarURL())
     embed.setTimestamp()
     embed.setDescription(`The commands are listed below:`)
     embed.addField("anti-swear", '`addword` | `delword` | `set-warn-msg` | `words`')
